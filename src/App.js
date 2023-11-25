a2fea4b5f8a3628350a23fc59ee4870d2b6557c0
jake-magri <jmagri1@uncc.edu>
2023-11-25 17:17:05 -0500

    fixed bug where display messge can show 'Displaying 10 of 1', increased memory and performance efficiency by restricting re-rendering

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import './App.css';
 
 const App = () => {
+  // State variables
   const [searchQuery, setSearchQuery] = useState('');
   const [emojis, setEmojis] = useState([]);
   const [displayedEmojis, setDisplayedEmojis] = useState([]);
@@ -11,10 +12,31 @@ const App = () => {
 
   const apiKey = '84fac0f20066123921007d0d48bac464759a4647';
 
+  // Event handler for search input change
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
   };
 
+  // Event handler for form submission
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    // Reset display count to default (5) or the maximum possible value below 5
+    setDisplayCount(Math.min(5, emojis.length));
+    fetchEmojis();
+  };
+
+  // Event handlers for "Show More" and "Show Less" buttons
+  const handleShowMore = () => {
+    // Increment display count, ensuring it doesn't exceed the total number of emojis
+    setDisplayCount((prevCount) => Math.min(prevCount + 5, emojis.length));
+  };
+
+  const handleShowLess = () => {
+    // Decrement display count, ensuring it doesn't go below 1
+    setDisplayCount((prevCount) => Math.max(prevCount - 5, 1));
+  };
+
+  // Fetch emojis from the API
   const fetchEmojis = useCallback(async () => {
     try {
       setLoading(true);
@@ -24,8 +46,10 @@ const App = () => {
       );
 
       if (Array.isArray(response.data)) {
+        // Filter and set the emojis based on the search query
         const filteredEmojis = filterEmojis(response.data, searchQuery);
         setEmojis(filteredEmojis);
+        // Set displayed emojis based on the current display count
         setDisplayedEmojis(filteredEmojis.slice(0, displayCount));
       } else {
         console.error('Invalid response format. Expected an array.');
@@ -37,6 +61,7 @@ const App = () => {
     }
   }, [searchQuery, apiKey, displayCount]);
 
+  // Function to filter emojis based on the search query
   const filterEmojis = (emojis, query) => {
     const filteredEmojis = emojis.filter((emoji) => {
       const emojiName = emoji.unicodeName.toLowerCase();
@@ -46,23 +71,12 @@ const App = () => {
     return filteredEmojis;
   };
 
-  const handleSearchSubmit = (event) => {
-    event.preventDefault();
-    fetchEmojis();
-  };
-
-  const handleShowMore = () => {
-    setDisplayCount((prevCount) => Math.min(prevCount + 5, emojis.length));
-  };
-
-  const handleShowLess = () => {
-    setDisplayCount((prevCount) => Math.max(prevCount - 5, 5));
-  };
-
+  // Fetch emojis on initial render and when search query changes
   useEffect(() => {
     fetchEmojis();
   }, [searchQuery, fetchEmojis]);
 
+  // Update displayed emojis when display count or emojis change
   useEffect(() => {
     setDisplayedEmojis(emojis.slice(0, displayCount));
   }, [displayCount, emojis]);
@@ -71,26 +85,30 @@ const App = () => {
     <div className="App">
       <h1>Emoji Search</h1>
       <form onSubmit={handleSearchSubmit}>
+        {/* Search input */}
         <input
           type="text"
           placeholder="Search emojis"
           value={searchQuery}
           onChange={handleSearchChange}
         />
+        {/* Search button */}
         <button type="submit">Search</button>
       </form>
+      {/* Loading or Displaying emojis section */}
       {loading ? (
         <p>Loading emojis...</p>
       ) : (
         <div>
-          <div>
+          {/* Display count and total emojis information */}
           <p>
-            Displaying {displayCount} of {emojis.length} emojis{' '}
+            Displaying {Math.min(displayCount, emojis.length)} of {emojis.length} emojis{' '}
           </p>
-        </div>
+          {/* Emoji list */}
           <ul>
             {displayedEmojis.map((emoji) => (
               <li key={emoji.slug}>
+                {/* Individual emoji item */}
                 <span>Emoji: </span>
                 <span>{emoji.character}</span>
                 <span> Name: </span>
@@ -98,12 +116,14 @@ const App = () => {
               </li>
             ))}
           </ul>
+          {/* Show More and Show Less buttons */}
           <p>
             <button onClick={handleShowMore}>Show More</button>{' '}
             <button onClick={handleShowLess}>Show Less</button>
           </p>
         </div>
       )}
+      {/* No emojis found message */}
       {displayedEmojis.length === 0 && <p>No emojis found</p>}
     </div>
   );
