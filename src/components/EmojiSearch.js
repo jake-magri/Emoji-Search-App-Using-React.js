6b955018fff4c42bd9a4adee727aca3e0cc5a215
jake-magri <jmagri1@uncc.edu>
2023-11-26 00:13:28 -0500

    optimized code for improved efficiency and readability

--- src/components/EmojiSearch.js
+++ src/components/EmojiSearch.js
@@ -3,40 +3,51 @@ import axios from 'axios';
 import EmojiList from './EmojiList';
 
 const EmojiSearch = ({ searchQuery, setSearchQuery }) => {
+  // State variables
   const [emojis, setEmojis] = useState([]);
   const [displayedEmojis, setDisplayedEmojis] = useState([]);
   const [loading, setLoading] = useState(true);
   const [displayCount, setDisplayCount] = useState(5);
   const [error, setError] = useState(null);
 
+  // API key for emoji API
   const apiKey = '84fac0f20066123921007d0d48bac464759a4647';
 
+  // Handler for input change in the search bar
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
   };
 
+  // Handler for form submission
   const handleSearchSubmit = (event) => {
     event.preventDefault();
+    // Form submission logic can be added here if needed
   };
 
+  // Memoized function to fetch emojis based on search query
   const fetchEmojis = useCallback(async () => {
     try {
       setLoading(true);
       setError(null);
 
+      // Build the API URL
       let apiUrl = `https://emoji-api.com/emojis?access_key=${apiKey}`;
       if (searchQuery.trim() !== '') {
         apiUrl += `&search=${searchQuery}`;
       }
 
+      // Fetch data using Axios
       const response = await axios.get(apiUrl);
 
+      // Logging for debugging
       console.log('API Response:', response.data);
       console.log('Search Query:', searchQuery);
 
+      // Handle the API response
       if (Array.isArray(response.data)) {
         const filteredEmojis = filterEmojis(response.data, searchQuery);
 
+        // Logging for debugging
         console.log('Filtered Emojis:', filteredEmojis);
 
         if (filteredEmojis.length === 0) {
@@ -66,6 +77,7 @@ const EmojiSearch = ({ searchQuery, setSearchQuery }) => {
     }
   }, [searchQuery, apiKey]);
 
+  // Function to filter emojis based on search query
   const filterEmojis = (emojis, query) => {
     const filteredEmojis = emojis.filter((emoji) => {
       const emojiName = emoji.unicodeName.toLowerCase();
@@ -75,19 +87,25 @@ const EmojiSearch = ({ searchQuery, setSearchQuery }) => {
     return filteredEmojis;
   };
 
+  // Effect to fetch data from the emoji API when searchQuery, apiKey, or displayCount changes
   useEffect(() => {
+    // Fetch data whenever the component mounts or when the searchQuery changes
     fetchEmojis();
+    // Reset display count to default when a new search is initiated
     setDisplayCount(5);
   }, [searchQuery, apiKey, fetchEmojis]);
 
+  // Effect to update displayed emojis whenever displayCount or emojis changes
   useEffect(() => {
     setDisplayedEmojis(emojis.slice(0, displayCount));
   }, [displayCount, emojis]);
 
+  // Handler for "Show More" button
   const handleShowMore = () => {
     setDisplayCount((prevCount) => Math.min(prevCount + 5, emojis.length));
   };
 
+  // Handler for "Show Less" button
   const handleShowLess = () => {
     setDisplayCount((prevCount) => Math.max(prevCount - 5, 4, 3, 2, 1));
   };
